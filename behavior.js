b596799e972ec75a0f8db0e1c350067a9fea7e96
Arneethar <62141171+Arneethar@users.noreply.github.com>
2021-06-26 13:41:29 +0100

    uploading website
    My ALX application

---
+++ behavior.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+	// write here
+	var thumbnailElement = document.getElementById("smart_thumbnail");
+	thumbnailElement.className;
+	if (thumbnailElement.className == "small") {
+	    // write here the code that will execute if the image is big
+	    thumbnailElement.className = "";
+	}
+	else{
+	    thumbnailElement.className = "small";
+	}
+    });
+});
